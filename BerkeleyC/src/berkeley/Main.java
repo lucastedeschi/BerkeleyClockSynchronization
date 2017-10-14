d68e31f47c8d1f4f0598f23418d23cb243227622
Lucas <Lucas@LTEDESCHI-DELL>
2017-10-14 11:58:17 -0300

    Criação do projeto Berkeley C

---
+++ BerkeleyC/src/berkeley/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package berkeley;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 41406133 - Lucas Rezende Tedeschi
+ * @author 41404998 - Jonatas Alvarenga Maximiano
+ */
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Porta 9997 - Inicilizar como slave(-s) ou mater(-m)?");
+        String entrada = sc.nextLine();
+        
+        if(entrada.equals("-m"))
+            new Master().initialize();
+        else if(entrada.equals("-s"))
+            new Slave().initialize();
+    }
+}
